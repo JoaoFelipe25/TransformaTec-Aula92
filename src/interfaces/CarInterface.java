ad9f2dec8bacd6bf8b553836f95200fbb3672c98
JoaoFelipe25 <joaofelipemarcelinodacruz@yahoo.com.br>
2022-07-19 16:41:48 -0300

    aula 92

--- src/interfaces/CarInterface.java
+++ src/interfaces/CarInterface.java
@@ -3,13 +3,48 @@ package interfaces;
 interface CarInterface {
     
     //método abstrato  - sem corpo
-    public void start();
-    public void accelerate();
-    public void brake();
+    public void start(String partida);
+    public void accelerate(String acelerando);
+    public void brake(String freiar);
 
 }
 
-// public class Car implements CarInterface{
+class Carr implements CarInterface{
+
+    @Override
+    public void start(String partida) {
+        
+        
+        System.out.println(partida);
+        
+    }
+
+    @Override
+    public void accelerate(String acelerando) {
+    
+        
+        System.out.println(acelerando);
+        
+    }
+
+    @Override
+    public void brake(String freiar) {
+        
+        System.out.println(freiar);
+        
+    }
+
+    public static void main(String[] args) {
+
+        Carr carr = new Carr();
+    
+        carr.start("brumm");
+        carr.accelerate("vrommmmmmmmmmmmmmmmm");
+        carr.brake("shhhhhhhhhhhhhhh");
+            
+    }
+    
+}
 
 
-// }
+
